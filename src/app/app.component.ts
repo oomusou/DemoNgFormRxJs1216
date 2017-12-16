60599fd33638f14e7585681dc99b9ed48748086d
Poy Chang <poypost@gmail.com>
2017-12-16 10:36:38 +0800

    清空

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,36 +1,6 @@
-import {AfterViewInit, Component, OnDestroy, ViewChild} from '@angular/core';
-import {NgControl} from '@angular/forms';
-import {Subscribable} from 'rxjs/Observable';
-import {takeUntil} from 'rxjs/operators';
-import {Subject} from 'rxjs/Subject';
-import {Subscription} from 'rxjs/Subscription';
+import {Component} from '@angular/core';
 
 @Component(
     {selector: 'app-root', templateUrl: './app.component.html', styles: []})
-export class AppComponent implements AfterViewInit, OnDestroy {
-  @ViewChild('searchRef') searchControl: NgControl;
-  sub: Subscription;
-  destroy$ = new Subject();
-
-  ngAfterViewInit(): void {
-    // 可以看到 ngModel 的內容和 FromControl 是一樣的
-    console.log(this.searchControl);
-
-    // 這個 Observable 不會停，所以要自己停，不然可能會有 memory leak 的問題
-    // 建議要停了話，可以透過 destroy$ 來處理，當有多個要停的時候，可以統一由
-    // destroy$ 來控制
-    this.sub = this.searchControl.valueChanges.pipe(takeUntil(this.destroy$))
-                   .subscribe(value => console.log(value));
-  }
-
-  ngOnDestroy(): void {
-    // 原本要這樣停掉
-    this.sub.unsubscribe();
-
-    // 透過 destroy$ 可以在這裡統一取消訂閱
-    this.destroy$.next();
-    // this.destroy$.unsubscribe();
-    // or
-    this.destroy$.complete();
-  }
+export class AppComponent {
 }
