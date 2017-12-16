bf5559d471b2abd4701b22653a904f7572864c4d
Poy Chang <poypost@gmail.com>
2017-12-16 10:46:12 +0800

    Template Form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,10 @@
 import {Component} from '@angular/core';
+import {FormGroup} from '@angular/forms/src/model';
 
 @Component(
     {selector: 'app-root', templateUrl: './app.component.html', styles: []})
 export class AppComponent {
+  send(form) {
+    console.log(form.value);
+  }
 }
